82915c296e2b9b6b6e3690e370300a7430458f4d
ShaziaMahar <shaziamahar478@gmail.com>
2024-06-04 12:20:57 +0500

    commit9

---
+++ class9.js
@@ -0,0 +1,76 @@
+
+// let person = {
+//     name :"shazzia",
+//     cnic : 4509767565,
+//     language : "sindhi",
+//     class :"javascript",
+//     speek : function (stu) {
+//         console.log(stu);
+//     }
+// }
+// console.log(person);
+
+// let person = {
+//     name :"shazzia",
+//     cnic : 4509767565,
+//     language : "sindhi",
+//     class :"javascript",
+//     speek : function (stu) {
+//         console.log(stu);
+//     }
+// }
+// console.log(person);
+
+// let person1 = {
+//     name :"shazzia",
+//     cnic : 4509767565,
+//     language : "sindhi",
+//     class :"javascript",
+//     speek : function (stu) {
+//         console.log(stu);
+//     }
+// }
+// let person1 = {
+//     name :"shazzia",
+//     cnic : 4509767565,
+//     language : "sindhi",
+//     class :"javascript",
+//     speek : function (stu) {
+//         console.log(stu + this.language);
+//     }
+// }
+
+// let person2 = {
+//     name :"shazzia",
+//     cnic : 4509767565,
+//     language : "sindhi",
+//     class :"javascript",
+//     speek : function (stu) {
+//         console.log(stu + this.name);
+//     }
+// }
+
+// // person1.speek("hellow world ");
+// // person2. speek("hellow world ");
+
+// // let student = [
+// //     person1 , person2
+// // ]
+// // console.log(student);
+    
+// function name1 () {
+//     return {
+//         name:prompt("enter your name"),
+//         cnic:+prompt("enter your cnic"),
+//         language:prompt("enter your language"),
+//         class:prompt("enter your class"),
+//         speek:function(stu) {
+            
+//         }
+//     }
+// }
+// let name = [name1()
+
+// ]
+// console.log(name);
+
